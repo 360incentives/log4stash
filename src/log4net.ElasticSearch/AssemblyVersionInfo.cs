a6ce459d9a2afb850bf3c07ddcf93457f7a58091
antonbrovkin <antbrov@gmail.com>
2023-08-22 21:25:51 +0300

    SB-2313 updated version

--- src/log4net.ElasticSearch/AssemblyVersionInfo.cs
+++ src/log4net.ElasticSearch/AssemblyVersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("23.8.15.0")]
-[assembly: AssemblyFileVersion("23.8.15.0")]
+[assembly: AssemblyVersion("23.8.22.0")]
+[assembly: AssemblyFileVersion("23.8.22.0")]
